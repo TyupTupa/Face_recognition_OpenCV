ac426d527b67fa9c76b48613b19df3201483744a
TyupTupa <marysik1596@gmail.com>
2024-11-15 23:07:13 +0300

    Added ReadME

---
+++ ReadME.md
@@ -0,0 +1,31 @@
+# Face and eyes recognition
+
+_using the OPENCV library._
+
+## Description 
+
+This program is designed to familiarize with the OpenCV library for Python. Using the Haar classifier, it detects a face on a video frame. And then, within the framework of the face coordinates, determines the presence of eyes.
+
+## Installation
+
+Install project using:
+
+```bash
+git clone https://github.com/TyupTupa/Face_recognition_OpenCV
+```
+Create Python virtual enviroment:
+
+```bash
+python -m venv venv
+```
+Activate:
+
+```bash
+venv\Scripts\activate
+```
+
+Run to find the desired requirements file and install it in your current environment:
+
+```bash
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-# https://habr.com/ru/companies/otus/articles/558426/
-# https://habr.com/ru/articles/547218/
-
 import cv2
-import argparse
 
+#pretrained models (Haar cascades) for face and eye reсognition
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+#capture camera with index 0
 cap = cv2.VideoCapture(0)
 
+#while the programm is working
 while True:
     ret, frame = cap.read()
 
+    #searches for faces on a frame
     faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5)
 
     for x, y, width, height in faces:
-        face_rec = cv2.rectangle(frame, (x, y), (x+width, y+height), color=(255, 10, 0), thickness=2)
+
+        #draws rectangle around each face
+        face_rec = cv2.rectangle(frame, (x, y), (x+width, y+height), color=(0, 10, 100), thickness=2)
+
+        #finds eyes on each face
         eyes = eye_cascade.detectMultiScale(frame[y:y+height, x:x+width], scaleFactor=1.3, minNeighbors=5)
+
+        #draws rectangles around each eye in current face
         for eye_x, eye_y, eye_width, eye_height in eyes:
             cv2.rectangle(frame[y:y+height, x:x+width], (eye_x, eye_y), (eye_x+eye_width, eye_y+eye_height), color=(255, 255, 0), thickness=2)
 
+    #shows the result on the video stream
     cv2.imshow('Face_recognition', frame)
-    
+
+    #exit conditions
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
         
